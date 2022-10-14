5010e5761c6fbdb122b7408f82d692d84698a0bf
Wilfaded <67386742+Wilfaded@users.noreply.github.com>
2022-10-15 01:46:11 +0700

    feat(localization): Add localization into architecture

---
+++ src/services/localization/index.ts
@@ -0,0 +1,20 @@
+import { initReactI18next } from "react-i18next";
+import i18next from "i18next";
+import languageDetector from "i18next-browser-languagedetector";
+import intervalPlural from "i18next-intervalplural-postprocessor";
+import Backend from "./backend";
+
+i18next
+    .use(initReactI18next)
+    .use(languageDetector)
+    .use(intervalPlural)
+    .use(Backend)
+    .init({
+        fallbackLng: "en",
+        whitelist: ["en", "ru"],
+        interpolation: {
+            escapeValue: false,
+        },
+    })
+
+export default i18next;
