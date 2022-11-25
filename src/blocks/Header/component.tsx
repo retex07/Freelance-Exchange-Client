682d8578c55b2a33bd069bcf0710d1f612c64834
AleksandrMol <sashamiasko@gmail.com>
2022-11-25 16:10:50 +0700

    feat: added profile, ads form (rebased)

--- src/blocks/Header/component.tsx
+++ src/blocks/Header/component.tsx
@@ -19,11 +19,15 @@ export default function Header() {
         <nav className="header-nav">
           <ul className="nav-list">
             <li className="nav-item">
-              <Link to={'/'} className='header-nav-link'>{t("mainLink")}</Link>
+              <Link to={'/'} className='header-nav-link'>
+                {t("mainLink")}
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link to={'/ads'} className='header-nav-link'>{t("adsLink")}</Link>
+              <Link to={'/ads'} className='header-nav-link'>
+                {t("adsLink")}
+              </Link>
             </li>
           </ul>
         </nav>
@@ -39,9 +43,15 @@ export default function Header() {
             <img className='header-dropdown' src={require("../../static/img/anon_ava.png")}/>
           } 
           drop={[
-            <Link to="/profile/page">{t("dropdown.profile")}</Link>,
-            <Link to="/myAds">{t("dropdown.ads")}</Link>,
-            <Link to="">{t("dropdown.exit")}</Link>,
+            <Link to="/profile/page">
+              {t("dropdown.profile")}
+            </Link>,
+            <Link to="/myAds">
+              {t("dropdown.ads")}
+            </Link>,
+            <Link to="">
+              {t("dropdown.exit")}
+            </Link>,
           ]}
         />
         {/* <Button
