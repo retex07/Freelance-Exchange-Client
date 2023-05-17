164c4ef4656855876ae8241ffeb4dd263eed9c87
Wilfaded <67386742+retex07@users.noreply.github.com>
2023-05-17 13:22:17 +0700

    refactor: Restyle Input component && Add some Props

--- src/components/Input/component.tsx
+++ src/components/Input/component.tsx
@@ -6,16 +6,49 @@ interface InputComponentProps {
   name: string;
   placeholder: string;
   label?: string;
+  register?: any;
+  error?: string;
+  required?: boolean;
 }
 
-export default function InputComponent({ type, name, placeholder, label }: InputComponentProps) {
+export default function InputComponent({
+  type,
+  required,
+  name,
+  placeholder,
+  label,
+  register,
+  error,
+}: InputComponentProps) {
   return (
     <div className="input-block">
       <label className="label" htmlFor={name}>
         {label}
       </label>
-
-      <input className="input" type={type} id={name} name={name} placeholder={placeholder} />
+      {error ? (
+        <>
+          <input
+            required={required}
+            className="input-error"
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            {...register}
+          />
+          <label className="label-error">{error}</label>
+        </>
+      ) : (
+        <input
+          className="input-normal"
+          required={required}
+          type={type}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          {...register}
+        />
+      )}
     </div>
   );
 }
