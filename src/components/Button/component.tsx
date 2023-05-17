cd1f1bb852eb19c7d53ec755ac919b96190f2436
Wilfaded <67386742+retex07@users.noreply.github.com>
2023-05-17 13:21:28 +0700

    refactor: Restyle && Add some Props

--- src/components/Button/component.tsx
+++ src/components/Button/component.tsx
@@ -8,14 +8,16 @@ interface IButtonProps {
   fullWidth?: boolean;
   disabled?: boolean;
   onClick?: () => void;
+  type?: 'submit' | 'reset' | 'button' | undefined;
 }
 
-export default function Button({ text, color, size, disabled, onClick, fullWidth }: IButtonProps) {
+export default function Button({ text, color, size, disabled, onClick, fullWidth, type }: IButtonProps) {
   return (
     <button
       disabled={disabled}
       className={`button button-${color} button-${size} ${fullWidth ? 'button-full' : undefined}`}
       onClick={onClick}
+      type={type}
     >
       {text}
     </button>
